ad7f10e9eef6745099058cb78e0f629d784d2558
Username234325 <jz2483@cornell.edu>
2025-12-02 17:54:29 -0500

    Update Factory.jl

--- src/Factory.jl
+++ src/Factory.jl
@@ -1,1 +1,33 @@
-throw(ErrorException("Oppps! No methods defined in src/Factory.jl. What should you do here?"))
+function build(modeltype::Type{MyExperimentalDrugCocktailContext}, data::NamedTuple)::MyExperimentalDrugCocktailContext
+    
+    # create new context instance -
+    context = modeltype() # model is empty instance
+
+    # populate context fields from data -
+    context.K = data.K
+    context.m = data.m
+    context.γ = data.γ
+    context.B = data.B
+    context.cost = data.cost
+    context.levels = data.levels
+    context.W = data.W
+    context.S = data.S
+    context.bounds = data.bounds
+
+    return context
+end
+
+function build(modeltype::Type{MyQLearningAgentModel}, data::NamedTuple)::MyQLearningAgentModel
+    
+    # create new model instance -
+    model = modeltype()
+
+    # populate model fields from data -
+    model.states = data.states
+    model.actions = data.actions
+    model.γ = data.γ
+    model.α = data.α
+    model.Q = data.Q
+
+    return model
+end
