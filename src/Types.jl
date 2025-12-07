a7ba51182f46e0313ded6dfa53bf474ba856032c
Username234325 <jz2483@cornell.edu>
2025-12-06 19:27:56 -0500

    Update Types.jl

--- src/Types.jl
+++ src/Types.jl
@@ -1,19 +1,6 @@
 abstract type MyAbstractContextModel end
 abstract type AbstractOnlineLearningModel end
 
-"""
-    mutable struct MyExperimentalDrugCocktailContext <: MyAbstractContextModel
-A mutable type for the experimental context of the drug combination design problem.
-
-### Fields
-- `K::Int64`: number of drug types
-- `m::Int64`: number of features per drug type
-- `γ::Array{Float64,1}`: effectiveness parameters
-- `μ::Array{Float64,1}`: mean parameters
-- `B::Float64`: total budget in USD
-- `cost::Dict{Int, Float64}`: maps drug type to cost per mg/kg
-- `levels::Dict{Int, NamedTuple}`: maps drug level to drug concentration in mg/kg
-"""
 mutable struct MyExperimentalDrugCocktailContext <: MyAbstractContextModel
     
     # initialize -
@@ -31,18 +18,6 @@ mutable struct MyExperimentalDrugCocktailContext <: MyAbstractContextModel
     MyExperimentalDrugCocktailContext() = new(); # create new *empty* instance 
 end
 
-"""
-    mutable struct MyQLearningAgentModel <: AbstractOnlineLearningModel
-
-A mutable type for the Q-Learning Agent model.
-
-### Fields
-- `states::Array{Int,1}`: array of states
-- `actions::Array{Int,1}`: array of actions
-- `γ::Float64`: discount factor
-- `α::Float64`: learning rate
-- `Q::Array{Float64,2}`: Q-value table
-"""
 mutable struct MyQLearningAgentModel <: AbstractOnlineLearningModel
 
     # data -
